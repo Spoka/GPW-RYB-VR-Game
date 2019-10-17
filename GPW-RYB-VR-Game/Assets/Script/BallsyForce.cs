cbd6b04e96e768bbf35a53d22ba11fdbdf4fd8c5
Spoka <thekikogy@gmail.com>
2019-10-17 13:33:27 +0100

    Repository created

---
+++ GPW-RYB-VR-Game/Assets/Script/BallsyForce.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BallsyForce : MonoBehaviour
+{
+    Rigidbody rigidBalldy;
+    Splitter splitter;
+    public GameObject ballsyPrefab;
+    public GameObject prefabLoc;
+    public float hTravel = 700;
+    public float lTravel = -1500;
+    bool shoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "CannonBall")
+        {
+            splitter = other.gameObject.GetComponent<Splitter>();
+            rigidBalldy = other.attachedRigidbody;
+            shoot = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "CannonBall")
+        {
+            shoot = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && shoot)
+        {
+            splitter.launched = true;
+            rigidBalldy.AddForce(new Vector3(0, hTravel, lTravel));
+            rigidBalldy.useGravity = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !shoot)
+        {
+            Instantiate(ballsyPrefab, prefabLoc.transform.position, ballsyPrefab.transform.rotation);
+        }
+    }
+}
---
+++ GPW-RYB-VR-Game/Assets/Script/Splitter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splitter : MonoBehaviour
+{
+    public float destructionTimer;
+    public bool launched;
+    float startTimer;
+    public int splitCase;
+    public GameObject splittedPrefab;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTimer = 0;
+    }
+    
+    public void Split()
+    {
+        switch(splitCase)
+        {
+            case 1:
+                Instantiate(splittedPrefab, transform.position, Quaternion.identity);
+                break;
+            case 2:
+                Instantiate(splittedPrefab, transform.position, Quaternion.identity);
+                Instantiate(splittedPrefab, transform.position + new Vector3(0,2,0), Quaternion.identity);
+                break;
+            case 3:
+                Instantiate(splittedPrefab, transform.position, Quaternion.identity);
+                Instantiate(splittedPrefab, transform.position + new Vector3(0, 2, 3), Quaternion.identity);
+                Instantiate(splittedPrefab, transform.position + new Vector3(3, 4, 0), Quaternion.identity);
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (launched)
+        {
+            startTimer += Time.deltaTime;
+            if(startTimer >= destructionTimer)
+            {
+                splitCase = Random.Range(1, 4);
+                Split();
+                launched = false;
+                Destroy(gameObject);
+                
+            }
+        }
+    }
+}
