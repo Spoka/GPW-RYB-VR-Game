fd07f3f25584ad7ae6484585eba6b1a8684bac11
Spoka <thekikogy@gmail.com>
2019-12-14 21:45:43 +0100

    .

--- GPW-RYB-VR-Game/Assets/Script/ConveyorSpawn.cs
+++ GPW-RYB-VR-Game/Assets/Script/ConveyorSpawn.cs
@@ -23,7 +23,7 @@ public class ConveyorSpawn : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (timerScript.GameTimer <= 180 && timerScript.GameTimer > 150)
         {
--- GPW-RYB-VR-Game/Assets/Script/GameTimerScript.cs
+++ GPW-RYB-VR-Game/Assets/Script/GameTimerScript.cs
@@ -10,6 +10,8 @@ public class GameTimerScript : MonoBehaviour
     public int GameTimer { get { return Mathf.RoundToInt(gameTimer); } }
 
     public Text gameTimerText;
+    public GameObject newGameCanvas;
+    public GameObject gameLights;
 
     protected bool gameOver;
     public bool GameOver { get { return gameOver; } }
@@ -17,17 +19,22 @@ public class GameTimerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameTimer = 180.0f;
+        
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         gameTimer -= Time.deltaTime;
-
         gameTimerText.text = "Time Left: " + GameTimer.ToString();
 
         if (GameTimer <= 0)
+        {
+            Time.timeScale = 0;
+            newGameCanvas.SetActive(true);
+            gameLights.SetActive(false);
             gameOver = true;
+            gameTimer = 180;
+        }
     }
 }
